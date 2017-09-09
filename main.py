b5726a409d4a22bdee335de1dd987c38949a0a2b
Raghavendra Vedula <vedularaghavendra03@gmail.com>
2017-09-10 02:11:30 +0530

    Initialize flask server

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, secure_filename
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+   return render_template('index.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+
+if __name__ == '__main__':
+   app.run(host = 'localhost', port = 5000)
